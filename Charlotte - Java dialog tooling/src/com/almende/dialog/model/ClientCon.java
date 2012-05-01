3c292ec23d07760308639ec39f8e7e305c27b475
Ludo Stellingwerff <ludo@almende.org>
2012-05-01 16:30:52 +0200

    Retry with trick from: http://www.oreillynet.com/onjava/blog/2007/01/singletons_and_lazy_loading.html

--- Charlotte - Java dialog tooling/src/com/almende/dialog/model/ClientCon.java
+++ Charlotte - Java dialog tooling/src/com/almende/dialog/model/ClientCon.java
@@ -6,19 +6,20 @@ import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class ClientCon {
-	private static Client client = null;
-	public static Client getClient(){
-		if (client == null){
-			synchronized(client){
-				if (client == null){
-					ClientConfig cc = new DefaultClientConfig();
-					cc.getProperties().put(ClientConfig.PROPERTY_THREADPOOL_SIZE,10);
-					client = Client.create(cc);
-					client.setConnectTimeout(1000);
-					client.setReadTimeout(10000);
-				}
-			}
+	private static class SingletonHandler {
+		static Client client = startClient();
+		private static Client startClient(){
+			
+			ClientConfig cc = new DefaultClientConfig();
+			cc.getProperties().put(ClientConfig.PROPERTY_THREADPOOL_SIZE,10);
+			Client client = Client.create(cc);
+			client.setConnectTimeout(1000);
+			client.setReadTimeout(10000);
+			return client;
 		}
-		return client;
+	}
+	
+	public static Client getClient(){
+		return SingletonHandler.client;
 	}
 }

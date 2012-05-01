83d0e16e7685f1a75a91edb045d866319134f8a0
Ludo Stellingwerff <ludo@almende.org>
2012-05-01 16:19:15 +0200

    Make sure client conn is not double initialized, takes too long:)

--- Charlotte - Java dialog tooling/src/com/almende/dialog/model/ClientCon.java
+++ Charlotte - Java dialog tooling/src/com/almende/dialog/model/ClientCon.java
@@ -9,11 +9,15 @@ public class ClientCon {
 	private static Client client = null;
 	public static Client getClient(){
 		if (client == null){
-			ClientConfig cc = new DefaultClientConfig();
-			cc.getProperties().put(ClientConfig.PROPERTY_THREADPOOL_SIZE,10);
-			client = Client.create(cc);
-			client.setConnectTimeout(1000);
-			client.setReadTimeout(10000);
+			synchronized(client){
+				if (client == null){
+					ClientConfig cc = new DefaultClientConfig();
+					cc.getProperties().put(ClientConfig.PROPERTY_THREADPOOL_SIZE,10);
+					client = Client.create(cc);
+					client.setConnectTimeout(1000);
+					client.setReadTimeout(10000);
+				}
+			}
 		}
 		return client;
 	}

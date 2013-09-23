d133d559b19792123b5dc9d2f9d74333379d4b34
shravanask <sshetty@ask-cs.com>
2013-09-23 17:25:40 +0200

    reformat. code tabbing

--- Charlotte - Java dialog tooling/src/com/almende/dialog/adapter/CMSmsServlet.java
+++ Charlotte - Java dialog tooling/src/com/almende/dialog/adapter/CMSmsServlet.java
@@ -1,14 +1,13 @@
 package com.almende.dialog.adapter;
 
-import java.io.IOException;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.almende.dialog.accounts.AdapterConfig;
 import com.almende.dialog.adapter.tools.CM;
 import com.almende.dialog.agent.tools.TextMessage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 
 public class CMSmsServlet extends TextServlet {
 
@@ -26,15 +25,15 @@ public class CMSmsServlet extends TextServlet {
 		return cm.sendMessage(message, subject, from, fromName, to, toName, config);
 	}
 	
-        @Override
-        protected int broadcastMessage( String message, String subject, String from, String fromName, String senderName,
-            Map<String, String> addressNameMap, AdapterConfig config ) throws Exception
-        {
-            String[] tokens = config.getAccessToken().split( "\\|" );
-    
-            CM cm = new CM( tokens[0], tokens[1], config.getAccessTokenSecret() );
-            return cm.broadcastMessage( message, subject, senderName, fromName, addressNameMap, config );
-        }
+    @Override
+    protected int broadcastMessage( String message, String subject, String from, String fromName, String senderName,
+        Map<String, String> addressNameMap, AdapterConfig config ) throws Exception
+    {
+        String[] tokens = config.getAccessToken().split( "\\|" );
+
+        CM cm = new CM( tokens[0], tokens[1], config.getAccessTokenSecret() );
+        return cm.broadcastMessage( message, subject, senderName, fromName, addressNameMap, config );
+    }
 
 	@Override
 	protected TextMessage receiveMessage(HttpServletRequest req, HttpServletResponse resp)

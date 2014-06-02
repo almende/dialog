6d1c3917850083d28621021c7eaf86ca076856fd
shravanask <sshetty@ask-cs.com>
2014-06-02 14:58:15 +0200

    update: added javadoc for media property keys

--- dialoghandler/src/main/java/com/almende/dialog/model/MediaProperty.java
+++ dialoghandler/src/main/java/com/almende/dialog/model/MediaProperty.java
@@ -3,7 +3,6 @@ package com.almende.dialog.model;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 
@@ -11,23 +10,46 @@ public class MediaProperty
 {
     public enum MediaPropertyKey
     {
-        TIMEOUT, //defines the timeout associated with the call
-        ANSWER_INPUT, //defines if the answer is given via dtmf, text etc
-        ANSWER_INPUT_MIN_LENGTH, //defines the length of th answer input. Typically dtmf
+        /**
+         * defines the timeout associated with the call
+         */
+        TIMEOUT,
+        /**
+         * defines if the answer is given via dtmf, text etc
+         */
+        ANSWER_INPUT,
+        /**
+         * defines the length of th answer input. Typically dtmf
+         */
+        ANSWER_INPUT_MIN_LENGTH,
         ANSWER_INPUT_MAX_LENGTH,
-        // defines a subtype for the question type. 
-        //E.g. open question with type: audio refers to voicemail
+        /**
+         * defines a subtype for the question type. <br>
+         * E.g. open question with type: audio refers to voicemail
+         */
         TYPE,
-        VOICE_MESSAGE_LENGTH, //defines the length of the voicemail to be recorded
-        //defines the number of times the question should repeat in case of a wrong answer input.
-        //works only for phonecalls so as to end a call with repeated input errors. 
+        /**
+         * defines the length of the voicemail to be recorded
+         */
+        VOICE_MESSAGE_LENGTH, 
+        /**
+         * defines the number of times the question should repeat in case of a wrong answer input.
+         * works only for phonecalls so as to end a call with repeated input errors.
+         */ 
         RETRY_LIMIT,
-        //boolean flag to indicate if a voice mssage start should be indicated by a beep or not
+        /**
+         * boolean flag to indicate if a voice mssage start should be indicated by a beep or not
+         */
         VOICE_MESSAGE_BEEP,
-        //boolean flag to indicate if the call should terminate when a dtmf is pressed
+        /**
+         * boolean flag to indicate if the call should terminate when a dtmf is pressed
+         */
         DTMF_TERMINATE,
-        //defines the speed at which the TTS is spoken by the TTS engine
+        /**
+         * defines the speed at which the TTS is spoken by the TTS engine
+         */
         TSS_SPEED;
+        
         @JsonCreator
         public static MediaPropertyKey fromJson(String name) {
             return valueOf(name.toUpperCase());

97da0084dfed87ae3ccb83b1a211c741d726df68
shravanask <sshetty@ask-cs.com>
2014-09-29 15:06:26 +0200

    fix: update TTS url from http to https

--- dialoghandler/src/main/java/com/almende/dialog/adapter/VoiceXMLRESTProxy.java
+++ dialoghandler/src/main/java/com/almende/dialog/adapter/VoiceXMLRESTProxy.java
@@ -1471,7 +1471,7 @@ public class VoiceXMLRESTProxy {
             e.printStackTrace();
             log.severe( e.getLocalizedMessage() );
         }
-        return "http://tts.ask-fast.com/api/parse?text=" + textForSpeech + "&lang=" + language
+        return "https://tts.ask-fast.com/api/parse?text=" + textForSpeech + "&lang=" + language
             + "&codec=" + contentType + "&speed=" + speed + "&format=" + format + "&type=.wav";
     }
     

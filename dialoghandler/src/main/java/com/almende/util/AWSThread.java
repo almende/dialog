e9335a045ef8dfccdd48d9f031bffb7ba4751598
sven4ask <sstam@ask-cs.com>
2015-03-26 17:45:07 +0100

    Put init back in aws client

--- dialoghandler/src/main/java/com/almende/util/AWSThread.java
+++ dialoghandler/src/main/java/com/almende/util/AWSThread.java
@@ -1,5 +1,6 @@
 package com.almende.util;
 
+import com.almende.dialog.Settings;
 import com.almende.dialog.aws.AWSClient;
 
 public class AWSThread extends Thread {
@@ -7,6 +8,7 @@ public class AWSThread extends Thread {
     @Override
     public void run() {
         ParallelInit.awsClient = new AWSClient();
+        ParallelInit.awsClient.init( Settings.BUCKET_NAME, Settings.AWS_ACCESS_KEY, Settings.AWS_ACCESS_KEY_SECRET );
         ParallelInit.awsClientActive = true;
     }
 }
